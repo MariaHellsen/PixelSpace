b35fb116eacb306d05c527ae358264f9f16cfbc2
Malin <maa.lin@outlook.com>
2025-02-05 14:43:52 +0100

    add css styling for introduction section on startpage

--- src/startpageContent.js
+++ src/startpageContent.js
@@ -1,18 +1,29 @@
 export const createHtmlStartpage = () => {
   const main = document.getElementById("main");
+  const introContainer = document.createElement("section");
+  introContainer.id = "introContainer";
   const headingLarge = document.createElement("h1");
   const headingSmall = document.createElement("h2");
   const introText = document.createElement("p");
 
-  headingLarge.innerHTML = "Webbyrå / Pixelspace";
+  headingLarge.innerHTML = "Webbyrå / PixelSpace";
   headingSmall.innerHTML = "Få bättre resultat med PixelSpace!";
   introText.innerHTML =
     "Vi är en fullservice webbyrå för allt digitalt i Stockholm. Med bred erfarenhet och flera digitala tjänster stärker vi din affär och ökar din försäljning så att du får fler kunder.";
 
   const btnReadMore = document.createElement("button");
+  btnReadMore.classList.add("btn", "btnSecondary");
   const btnContact = document.createElement("button");
+  btnContact.classList.add("btnPrimary", "btn");
   btnReadMore.innerHTML = "Läs mer";
   btnContact.innerHTML = "Kontakta oss";
 
-  main.append(headingLarge, headingSmall, introText, btnReadMore, btnContact);
+  introContainer.append(
+    headingLarge,
+    headingSmall,
+    introText,
+    btnReadMore,
+    btnContact
+  );
+  main.appendChild(introContainer);
 };
